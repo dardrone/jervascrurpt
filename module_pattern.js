bfc49aa13f4225eb911060a233a269324cc9e1f6
Darren Jennings <darren@educents.com>
2016-08-01 22:17:15 -0700

    Added module pattern js.

---
+++ module_pattern.js
@@ -0,0 +1,21 @@
+var MakeMoreExciting = (function () {
+
+  	function addStrings(sentence, characters){
+		return sentence + characters;
+	};
+  
+	return {
+	    excitingify: function (sentence) {
+	    	// I can call `privateMethod()` you know...
+			return addStrings(sentence, '!!!!');
+		},
+		excitingifyN: function(sentence, n){
+			return addStrings(sentence, Array(n+1).join('!'));
+		}
+	}
+})();
+
+var myModule = MakeMoreExciting;
+console.log(myModule.excitingify('Cats'));			// Cats!!!!!!!!!!
+console.log(myModule.excitingifyN('Cats', 10)); 		// Cats!!!!!!!!!!
+console.log(myModule.addStrings('Cats', '!!!!')); 	// Uncaught TypeError: MyModule.addStrings is not a function
